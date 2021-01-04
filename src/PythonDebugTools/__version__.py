289f920b4e3088417bf7e5b26cd6669ab6243eb0
Tyler Stegmaier <jakar510@gmail.com>
2021-01-04 17:09:21 -0600

    updates

--- src/PythonDebugTools/__version__.py
+++ src/PythonDebugTools/__version__.py
@@ -5,7 +5,7 @@
 # ------------------------------------------------------------------------------
 
 # major.minor[.maintenance]
-VERSION = (2, 2, 2)
+VERSION = (2, 2, 3)
 
 version = '.'.join(map(str, VERSION))
 
